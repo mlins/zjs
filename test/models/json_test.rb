9470e3af5a8935e6cc33a429868d2cd5eaf49aac
Matt Lins <mattlins@gmail.com>
2019-11-01 14:37:25 -0500

    Add more tests for json model

--- test/models/json_test.rb
+++ test/models/json_test.rb
@@ -1,12 +1,32 @@
 require "minitest/autorun"
 require_relative "../../app/models/json"
 
-class JsonTest < Minitest::Test
-  def setup
-    @json = Models::Json.new(%Q([{"key": "value"}, {"key": "value"}]))
-  end
+module Models
+  class JsonTest < Minitest::Test
+    def setup
+      @raw_json = %Q([{"key": "value"}, {"key": "value"}])
+      @json = Models::Json.new(@raw_json)
+    end
 
-  def test_unique_keys
-    assert_equal ["key"], @json.unique_keys
+    def test_raw_json
+      assert_equal @raw_json, @json.raw_json
+    end
+
+    def test_data
+      data = [
+        {
+          "key" => "value"
+        },
+        {
+          "key" => "value"
+        }
+      ]
+
+      assert_equal data, @json.data
+    end
+
+    def test_unique_keys
+      assert_equal ["key"], @json.unique_keys
+    end
   end
 end
